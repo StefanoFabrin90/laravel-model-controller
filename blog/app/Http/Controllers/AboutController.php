c38efecfb24b565e578416484bdc62cea9085124
stef_fab@hotmail.it <stef_fab@hotmail.it>
2022-01-21 15:38:19 +0100

    add 2

--- blog/app/Http/Controllers/AboutController.php
+++ blog/app/Http/Controllers/AboutController.php
@@ -3,12 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Movie;
 
 class AboutController extends Controller
 {
     //aboutpage
     public function index()
     {
-        return view('about');
+        $movies = Movie::all();
+        //dump($movies);
+
+
+        return view('about', compact('movies'));
     }
 }
--- blog/resources/views/about.blade.php
+++ blog/resources/views/about.blade.php
@@ -2,6 +2,22 @@
 
 @section('content')
     <div class="container">
-        <h1 class="mb-5">About page</h1>
+        <h1 class="mb-5">Movie</h1>
+        <section>
+            @foreach($movies as $movie)
+                <h4>{{ $movie->title }}</h4>
+                <ul>
+                    <li>{{ $movie->original_title }}</li>
+                    <li>{{ $movie->nationality }}</li>
+                    <li>{{ $movie->date }}</li>
+                    <li>{{ $movie->vote }}</li>
+                </ul>
+    
+                @if(!$loop->last)
+                    <hr>
+                @endif
+    
+            @endforeach
+        </section>
     </div>
 @endsection
